bf1e938014b5118492526d974743daa6cae599ca
Rafael Perez <prafael16@gmail.com>
2022-02-21 01:25:04 -0400

    Save alexa rank

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,4 +1,17 @@
 class Domain < ApplicationRecord
   has_many :short_urls
   validates :url, presence: true, uniqueness: true
+  before_create :set_alexa_rank
+
+  private
+    def set_alexa_rank
+      url = URI.parse("https://awis.api.alexa.com/api?Action=UrlInfo&ResponseGroup=Rank&Output=json&Url=#{self.url}")
+      req = Net::HTTP::Get.new(url.to_s)
+      req.add_field('x-api-key', ENV['ALEXA_API_KEY'])
+      res = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') {|http| http.request(req) }
+      
+      response = JSON.parse(res.body)
+      self.alexa_rank = response&.[]('Awis')&.[]('Results')&.[]('Result')&.[]('Alexa')&.[]('TrafficData')&.[]('Rank') || ""
+    end
 end
+     
--- app/views/short_urls/_short_url.json.jbuilder
+++ app/views/short_urls/_short_url.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! short_url, :url, :short_code, :visits
 json.short_url redirect_url(short_code: short_url.short_code)
+json.alexa_rank short_url.domain.alexa_rank
